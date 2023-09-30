fb81cf2f2157239d9761c01c00a4cc7609c28229
MGonzalesZ <mauriciogzb1322@gmail.com>
2023-09-29 23:18:37 -0400

    configuracion de fecha - props en constantes

--- src/App.js
+++ src/App.js
@@ -32,6 +32,9 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      {/* ALTERNATIVA
+      <ExpenseItem expense={expenses[0]}></ExpenseItem> */}
+
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,10 +3,19 @@ import './ExpenseItem.css';
 function ExpenseItem(props) {
   // USO DE PARAMETROS
   // PROPS ES EL OBJETO QUE LLEVA TODOS LOS CAMPOS
+
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   return (
     <div className="expense-item">
       {/* OJO AL SER UN OBJETO FECHA SE DEBE CONVERTIR A ISOString */}
-      <div>{props.date.toISOString()}</div>
+      {/* <div>{props.date.toISOString()}</div> */}
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
 
