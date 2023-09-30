6a9044247ce9a6ae15812a9c01e4a2c53ab27ae5
MGonzalesZ <mauriciogzb1322@gmail.com>
2023-09-29 23:36:45 -0400

    Fecha en componente separado

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,15 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
   // USO DE PARAMETROS
   // PROPS ES EL OBJETO QUE LLEVA TODOS LOS CAMPOS
 
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   return (
     <div className="expense-item">
       {/* OJO AL SER UN OBJETO FECHA SE DEBE CONVERTIR A ISOString */}
       {/* <div>{props.date.toISOString()}</div> */}
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
 
