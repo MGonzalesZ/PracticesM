ff864b8981ee75da149f8d25cf8aaac4688bcfb0
MGonzalesZ <mauriciogzb1322@gmail.com>
2023-10-02 11:18:25 -0400

    cambio de ExpenseItem a stateless

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -14,13 +14,14 @@ const ExpenseItem = (props) => {
   // React no reinicializa el State...por eso el valor cambia! aun al reejecutarse nuevamente
   // al componente con el setTitle()
 
-  const [title, setTitle] = useState(props.title);
-  console.log('Expense Item reevaluated by React');
+  // SOLO PARA DEMO
+  // const [title, setTitle] = useState(props.title);
+  // console.log('Expense Item reevaluated by React');
 
-  const clickHandler = () => {
-    setTitle('Updated');
-    console.log(title);
-  };
+  // const clickHandler = () => {
+  //   setTitle('Updated');
+  //   console.log(title);
+  // };
 
   // USO DE PARAMETROS
   // PROPS ES EL OBJETO QUE LLEVA TODOS LOS CAMPOS
@@ -29,10 +30,11 @@ const ExpenseItem = (props) => {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
+        {/* <h2>{title}</h2> */}
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 };
