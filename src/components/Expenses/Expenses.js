43f738e8d7b1de3a6c5cdeeebb69808f1622f3fd
MGonzalesZ <mauriciogzb1322@gmail.com>
2023-10-02 18:22:41 -0400

    mostrar contenido segun condicion (no expenses) en Expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -26,6 +26,20 @@ const Expenses = (props) => {
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+
+  let expensesContent = <p>No expenses found</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((elemento) => {
+      return (
+        <ExpenseItem
+          key={elemento.id}
+          title={elemento.title}
+          amount={elemento.amount}
+          date={elemento.date}
+        />
+      );
+    });
+  }
   return (
     <Card className="expenses">
       <ExpensesFilter
@@ -33,36 +47,8 @@ const Expenses = (props) => {
         onChangeFilter={filterChangeHandler}
       />
       <p>Data for years {filterInfoText} is hidden</p>
-      {filteredExpenses.map((elemento) => {
-        return (
-          <ExpenseItem
-            key={elemento.id}
-            title={elemento.title}
-            amount={elemento.amount}
-            date={elemento.date}
-          />
-        );
-      })}
-      {/* <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      /> */}
+      {/* AQUI SE DEFINE POR EL CONDICIONAL QUE SE VA A RENDERIZAR */}
+      {expensesContent}
     </Card>
   );
 };
